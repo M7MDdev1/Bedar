6e517d863b419154b40ea99ada2f0338b59dc69b
Mohammed27800 <asmte2780@gmail.com>
2024-09-02 10:53:44 +0300

    Fix NavBar

---
+++ src/Components/SideBar.tsx
@@ -0,0 +1,67 @@
+import bedar from "../assets/Bedar.svg";
+import NavigationLinks from "./NavigationLinks";
+
+interface SideBarProps {
+  visible: boolean;
+  setVisibility: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function SideBar({ visible, setVisibility }: SideBarProps) {
+  const Links = [
+    {
+      title: "الرئيسية",
+      to: "/",
+    },
+    {
+      title: "من نحن",
+      to: "/AboutUs",
+    },
+    {
+      title: "الوثائق",
+      to: "/Documents",
+    },
+    {
+      title: "بحوث ودراسات",
+      to: "/ResearchesAndStudys",
+    },
+    {
+      title: "ساهم معنا",
+      to: "/JoinUs",
+    },
+    {
+      title: "تواصل معنا",
+      to: "/ContactUs",
+    },
+    {
+      title: "قالوا عنا",
+      to: "/SaidAboutUs",
+    },
+    {
+      title: "تسجيل دخول",
+      to: "/Auth",
+    },
+  ];
+  return (
+    <div
+      className={`absolute flex w-full h-[97%]  flex-row-reverse ${
+        !visible && " -z-10 translate-x-full"
+      } duration-500 h-screen bottom-0 right-0 font-bahij`}
+    >
+      <div className={"w-full bg-secondary rounded-tl-[5.5rem]"}>
+        <div className={"p-10 flex flex-col items-center"}>
+          <img src={bedar} alt="Bedar.svg" className="w-[7.5rem]" />
+
+          <NavigationLinks
+            links={Links}
+            className={
+              "text-white h-72 flex flex-col justify-between items-center"
+            }
+          />
+        </div>
+      </div>
+      <button
+        className={`bg-transparent w-full`}
+        onClick={() => setVisibility(false)}
+      />
+    </div>
+  );
+}
--- src/Container/NavBar.tsx
+++ src/Container/NavBar.tsx
@@ -1,5 +1,5 @@
 import { FiMenu } from "react-icons/fi";
-import bedar from "../assets/Bedar.svg";
+import bedar from "../assets/BedarShadow.png";
 interface NavBarProps {
   SideBarVisibility: boolean;
   setSideBarVisibility: React.Dispatch<React.SetStateAction<boolean>>;
@@ -9,16 +9,17 @@ export default function NavBar({
   setSideBarVisibility,
 }: NavBarProps) {
   return (
-    <div className="bg-primary relative overflow-hidden h-28 p-2 flex justify-evenly items-center flex-row-reverse rounded-b-2xl shadow-xl ">
+    <div className="bg-primary h-[7.188rem] p-4 relative rounded-b-2xl flex flex-row-reverse items-center">
       <FiMenu
         onClick={() => setSideBarVisibility(!SideBarVisibility)}
-        className={`text-white hover:text-gray-500 transition-all`}
+        className={`text-white hover:text-gray-500`}
         size={30}
       />
-      <div className={"w-[7.5rem] select-none"}>
-        <img src={bedar} alt="Bedar.svg" />
-      </div>
-      <div></div>
+      <img
+        src={bedar}
+        alt="Bedar.png"
+        className="w-[11.25rem] absolute select-none object-cover left-1/2 transform -translate-x-1/2"
+      />
     </div>
   );
 }
