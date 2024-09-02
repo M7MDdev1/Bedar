5df52aa0ec575198293037656ab62ad512a300d5
7t4o <hatemlaptop1@gmail.com>
2024-09-02 20:08:07 +0300

    make icons work in bottom bar

--- src/Container/Footer.tsx
+++ src/Container/Footer.tsx
@@ -40,7 +40,15 @@ export default function Footer() {
         className={" w-[12.625rem] mx-auto -translate-y-3 object-cover"}
       />
       <div>
-        <SocailMediaButton className={" -translate-y-6"} />
+        <SocailMediaButton 
+          className={" -translate-y-6"}
+          links={{
+            twitter: "https://x.com/BedarYouth",
+            instagram: "https://www.instagram.com/bedaryouth",
+            youtube: "https://www.youtube.com/@BEDARYOUTH",
+            linkedin: "https://www.linkedin.com/company/bedar-for-youth-development/",
+          }} 
+        />
 
         <NavigationLinks
           links={Links}
