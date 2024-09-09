cf2500176240ef36f735a9241b61445ce906eeb8
7t4o <hatemlaptop1@gmail.com>
2024-09-09 19:41:34 +0300

    add background logo to the home page

---
+++ src/Components/BackgroundLogo.tsx
@@ -0,0 +1,8 @@
+import logo from '../assets/BackLogo.png';
+export default function BackgroundLogo() {
+    return (
+        <div className="h-80 overflow-hidden flex justify-center">
+            <img src={logo} className='w-full object-cover object-bottom'></img>
+        </div>
+    );
+}
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 export default function Card({ icon: Icon, title, description }: CardProps) {
     return (
-        <div className="w-[78%] min-h-44 bg-white p-6 rounded-xl drop-shadow-xl mb-6 max-w-sm text-center">
+        <div className="w-[78%] min-h-44 bg-white p-6 rounded-xl drop-shadow-xl max-w-sm text-center">
             <div className="flex justify-center items-center text-3xl mb-4 text-yellow-600 font-light"><Icon /></div>
             <h3 className="text-xl font-bold mb-2">{title}</h3>
             <p className="text-xs text-black ">{description}</p>
