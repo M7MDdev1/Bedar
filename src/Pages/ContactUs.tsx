7da7d4e0da2b28d2feefa5316fcd4a1c8e01656d
7t4o <hatemlaptop1@gmail.com>
2024-09-23 09:39:15 +0300

    Convert the form to Formik

--- src/Pages/ContactUs.tsx
+++ src/Pages/ContactUs.tsx
@@ -1,4 +1,6 @@
 import bedar from '../assets/Contact_BedarLogo.png';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { IoLocationSharp } from "react-icons/io5";
 import { MdLocalPhone } from "react-icons/md";
 import { HiEnvelope } from "react-icons/hi2";
@@ -7,20 +9,23 @@ import { HiEnvelope } from "react-icons/hi2";
 export default function ContactUs() {
   const Fields = [
     {
+      id: "name",
       name: "الاسم",
       type: "text"
     },
     {
+      id: "email",
       name: "البريد الإلكتروني",
       type: "email"
     },
     {
+      id: "title",
       name: "عنوان الرسالة",
       type: "text"
     },
   ]
 
-  const Info = [
+  const FooterInfo = [
     {
       icon: <IoLocationSharp/>,
       title: "جازان، حي الشاطئ",
@@ -40,22 +45,50 @@ export default function ContactUs() {
   return (
     <div className="flex flex-col items-center">
       <h2 className="my-6 text-2xl font-bold">تواصل معنا</h2>
-      <form className="w-full bg-[#C58F3526] flex flex-col text-right p-4 pb-12 relative">
-        {Fields.map((field) => (
-          <>
-            <label className="m-3">{field.name}</label>
-            <input type={field.type} className="h-12 rounded-2xl border border-[#c58f35ad]"/>
-          </>
-        ))}
-        <label className="m-3">محتوى الرسالة</label>
-        <textarea className="h-[192px] rounded-2xl border border-[#c58f35ad]"/>
-        <img src={bedar} className='absolute -z-50 w-60 top-[54%] left-[50%] -translate-x-1/2 -translate-y-1/2' />
-      </form>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          title: "",
+          content: ""
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("Name is required"),
+          email: Yup.string().email("Invalid email address").required("Email is required"),
+          title: Yup.string().required("Title is required"),
+          content: Yup.string().required("Content is required")
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form  className="w-full bg-[#C58F3526] flex flex-col text-right p-4 relative">
+            {Fields.map((field) => (
+              <>
+                <label htmlFor={field.id} className="m-3">{field.name}</label>
+                <Field type={field.type} id={field.id} name={field.id} className="h-12 rounded-2xl p-4 text-right border border-[#c58f35ad]"/>
+                <ErrorMessage name={field.id} component="div"/>
+              </>
+            ))}
+            <>
+              <label htmlFor='content' className="m-3">محتوى الرسالة</label>
+              <Field as="textarea" id='content' name='content' className="h-[192px] rounded-2xl p-4 text-right border border-[#c58f35ad]"/>
+            </>
+            <button type='submit' disabled={isSubmitting} className='w-48 h-14 my-12 mx-auto bg-[#C58F35] text-white rounded-[20px] shadow-[2px_2px_5px_0px_rgba(197,143,53,0.68)]'>
+              {isSubmitting ? "...جاري التسجيل" : "تسجيل"}
+            </button>
+            <img src={bedar} className='absolute -z-50 w-60 top-[54%] left-[50%] -translate-x-1/2 -translate-y-1/2' />
+          </Form>
+        )}
+      </Formik>
+
 
       {/* Footer of Contact */}
       <div className="w-full flex flex-col items-end text-right p-4 pb-12 font-bold gap-5">
         <p className='w-52'>كما نسعد بزيارتكم وتواصلكم على بياناتنا أدناه</p>
-        {Info.map((In) => (
+        {FooterInfo.map((In) => (
           <div className='flex flex-row-reverse text-sm items-center gap-2'>
             <div className='text-[#C58F35] rounded-full border border-[#c58f35ad] p-1'>{In.icon}</div>
             <a href={In.src}>{In.title}</a>
